0dcdff128fc17dac478682cbfd7632e67ee0fd5e
Jonatan Heyman <jonatan@heyman.info>
2018-11-23 13:45:38 +0100

    Automatically use project specific history file if we’re in a git repository

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,24 +1,30 @@
 def setup_history():
-  import atexit
-  import os
-  import readline
-  import rlcompleter
-  import sys
+    import atexit
+    import os
+    import readline
+    import rlcompleter
+    import sys
+    import subprocess
 
-  # change autocomplete to tab
-  readline.parse_and_bind("tab: complete")
+    # change autocomplete to tab
+    readline.parse_and_bind("tab: complete")
+    
+    # see if we're in a git repo
+    repo_dir = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE, stderr=open(os.devnull, "w")).communicate()[0].rstrip().decode('utf-8')
+    if repo_dir:
+        history_path = os.path.join(repo_dir, ".pyhistory")
+    else:
+        history_path = os.path.expanduser("~/.pyhistory")
 
-  history_path = os.path.expanduser("~/.pyhistory")
+    def save_history(history_path=history_path):
+        import readline
+        readline.set_history_length(10000)
+        readline.write_history_file(history_path)
 
-  def save_history(history_path=history_path):
-      import readline
-      readline.set_history_length(10000)
-      readline.write_history_file(history_path)
+    if os.path.exists(history_path):
+        readline.read_history_file(history_path)
 
-  if os.path.exists(history_path):
-      readline.read_history_file(history_path)
-
-  atexit.register(save_history)
+    atexit.register(save_history)
 setup_history()
 del setup_history
 
